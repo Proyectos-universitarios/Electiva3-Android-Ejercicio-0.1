2a78de9c0c605dd1b1f163f88748c11bf7f67ba5
Roberto Perdomo <rperdomo32@gmail.com>
2021-02-21 15:51:30 -0600

    Exercice 2 and 3 using excel

--- app/src/main/java/com/example/electiva3tareas01/MainActivity.java
+++ app/src/main/java/com/example/electiva3tareas01/MainActivity.java
@@ -1,15 +1,18 @@
 package com.example.electiva3tareas01;
 
 import android.app.Activity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
  ListView lv;
- String[] Categorias = {"CARNE", "VERDURAS", "POLLO", "ARROZ", "TOMATES", "GUTAS", "PASTELES", "MASCOTAS", "MASCOTAS 2 ", "DULCERIAS", "BEBES"};
+ String[] Categorias;
 
 
     @Override
@@ -17,12 +20,19 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Categorias = getResources().getStringArray(R.array.categorias);
+
         lv = (ListView) findViewById(R.id.ListViewid);
         ArrayAdapter<String> itemsAdapter =
                 new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Categorias);
-
-
         lv.setAdapter(itemsAdapter);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "USTED A SELECCIONADO LA CATEGORIA "+ Categorias[position], Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
