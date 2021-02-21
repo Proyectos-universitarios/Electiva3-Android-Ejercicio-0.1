993f13affa9e8395522a6698b0edaa7cb77a0d96
Roberto Perdomo <rperdomo32@gmail.com>
2021-02-21 15:24:00 -0600

    New exarcice

--- app/src/main/java/com/example/electiva3tareas01/MainActivity.java
+++ app/src/main/java/com/example/electiva3tareas01/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.electiva3tareas01;
 
+import android.app.Activity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+ ListView lv;
+ String[] Categorias = {"CARNE", "VERDURAS", "POLLO", "ARROZ", "TOMATES", "GUTAS", "PASTELES", "MASCOTAS", "MASCOTAS 2 ", "DULCERIAS", "BEBES"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lv = (ListView) findViewById(R.id.ListViewid);
+        ArrayAdapter<String> itemsAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Categorias);
+
+
+        lv.setAdapter(itemsAdapter);
+
     }
 }
